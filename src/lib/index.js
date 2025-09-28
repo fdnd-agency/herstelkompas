0d529d8891597975c2c2f34ed23fd16c5c157b47
DivaniNL <dylanvannierop@gmail.com>
2025-09-28 16:17:15 +0200

    #31 made basic routes and files for client enviroment + basic setup for admin

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,5 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+
+export { default as Bingocard } from './components/Bingocard.svelte'
+export { default as Sidebar } from './components/Sidebar.svelte'
