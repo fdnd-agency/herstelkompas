4e6907ec44ab313f54afc2fba29c0607dba812eb
DivaniNL <dylanvannierop@gmail.com>
2025-12-08 08:44:59 +0100

    moved vragenlijst into correct component folders

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,15 +3,15 @@ export { default as Bingocard } from './components/Bingocard.svelte'
 export { default as Logo } from './components/Logo.svelte'
 export { default as Treatment } from './components/Treatment.svelte'
 export { default as Header } from './components/Header.svelte'
-export { default as Vragenlijst } from './components/Vragenlijst.svelte'
-export { default as Vraag } from './components/Vraag.svelte'
+
+
 
 // Organisms
 export { default as Sidebar } from './components/organisms/Sidebar.svelte'
-
+export { default as Vragenlijst } from './components/organisms/Vragenlijst.svelte'
 // Molecules
 export { default as SidebarNavigation } from './components/molecules/SidebarNavigation.svelte'
 export { default as AuthInformation } from './components/molecules/AuthInformation.svelte'
-
+export { default as Vraag } from './components/molecules/Vraag.svelte'
 // Atoms
 export { default as SidebarLink } from './components/atoms/SidebarLink/SidebarLink.svelte'
