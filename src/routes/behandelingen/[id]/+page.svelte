9b631c76f7d6984c8b019ce7ad1589d0328d4cd6
Kerem <90683756+Kerem-Tutucu@users.noreply.github.com>
2025-12-12 15:41:47 +0100

    feat: add animated conic-gradient background to body
    feat: add animated conic-gradient background to body #152

--- src/routes/behandelingen/[id]/+page.svelte
+++ src/routes/behandelingen/[id]/+page.svelte
@@ -9,3 +9,48 @@
   activiteit={data.activiteit}
   Questionlist={Questionlist}
 />
+
+<style>
+  :global(body) {
+    margin: 0;
+    height: 100dvh;
+
+    background-image:
+      repeating-conic-gradient(
+        from var(--angle1, 0deg),
+        transparent 0 3.5%, 
+        rgb(245, 122, 136) 0 5%
+      ),
+      repeating-conic-gradient(
+        from var(--angle, 0deg),
+        lightpink 0 1.5%, 
+        deeppink 0 5%
+      );
+
+    animation:
+      draai 10s linear infinite,
+      draai1 10s linear infinite;
+  }
+
+  @keyframes draai {
+    0% { --angle: 0deg; }
+    100% { --angle: 360deg; }
+  }
+
+  @keyframes draai1 {
+    0% { --angle1: 0deg; }
+    100% { --angle1: -360deg; }
+  }
+
+  @property --angle {
+    syntax: "<angle>";
+    initial-value: 0deg;
+    inherits: false;
+  }
+
+  @property --angle1 {
+    syntax: "<angle>";
+    initial-value: 0deg;
+    inherits: false;
+  }
+</style>
