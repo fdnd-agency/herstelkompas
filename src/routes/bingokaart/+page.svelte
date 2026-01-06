df571a01ca3c5e711bd5910daa9d84db091c498e
Ties7 <97968332+Ties7@users.noreply.github.com>
2026-01-06 20:23:26 +0100

    feat: send button animation #64

--- src/routes/bingokaart/+page.svelte
+++ src/routes/bingokaart/+page.svelte
@@ -1,7 +1,18 @@
 <script>
-    let { data } = $props();
+    let { data, form } = $props();
+
     let behandelingen = data.behandelingen
     let bingokaart = data.bingokaart
+
+    let success = $state(false)
+
+    if(form?.success) {
+        success = true
+        setTimeout(() => {
+            success = false
+        }, 1000)
+    }
+
     import { Bingocard } from '$lib';
     import { Sidebar } from '$lib';
     import { page } from '$app/state';
@@ -10,6 +21,6 @@
     <Sidebar/>
     <main>
         <h1>Bingokaart pagina</h1>
-        <Bingocard {bingokaart}/>
+        <Bingocard {bingokaart} {success} />
     </main>
 </div>
