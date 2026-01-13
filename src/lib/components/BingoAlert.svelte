202217c1876216ebbfc2fd63b25b8c8a02483bed
Ties7 <97968332+Ties7@users.noreply.github.com>
2026-01-13 12:33:56 +0100

    fix: Progressive Enhancement dialog popup #96

--- src/lib/components/BingoAlert.svelte
+++ src/lib/components/BingoAlert.svelte
@@ -1,50 +1,50 @@
 <script>
-    const { count, prizes } = $props(); //aantal afgekruisde vakjes
-    
-    // let dialog = $state();
-    let message = $state('');
-    let src = $state('');
-    let alt = $state('');
-    let dialog = $state(null);
-    let prize = $state(null);
+    const { count, prizes } = $props();
 
-    $effect(() => {
-        prize = prizes.find(p => p.Hoeveelheid === count) ?? null;
-
-        if (prize && dialog) {
-            dialog.showModal();
-        }
-    });
+    const prize = prizes.find(p => p.Hoeveelheid === count) ?? null;
 </script>
 
-<dialog bind:this={dialog}> <!-- hiermee word de dialog gedefineerd en heeft het dialog eigenschappen -->
+{#if prize}
+<dialog open>
     <h2>Bingo!</h2>
-    {#if prize}
-        <p>Je wint een <strong>{prize.titel}</strong>!</p>
-    {/if}
+
+    <p>Je wint een <strong>{prize.titel}</strong>!</p>
+
+    <!-- form method="dialog" werkt ook zonder JS -->
     <form method="dialog">
         <button type="submit">
             Sluiten
-            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15"><path fill="#fff" d="M10.969 3.219a.574.574 0 1 1 .812.812L8.313 7.5l3.468 3.469l.074.09a.575.575 0 0 1-.796.796l-.09-.074L7.5 8.312l-3.469 3.47a.574.574 0 1 1-.812-.813L6.688 7.5l-3.47-3.469l-.073-.09a.575.575 0 0 1 .796-.797l.09.075L7.5 6.687z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15">
+                <path fill="#fff" d="M10.969 3.219a.574.574 0 1 1 .812.812L8.313 7.5l3.468 3.469l.074.09a.575.575 0 0 1-.796.796l-.09-.074L7.5 8.312l-3.469 3.47a.574.574 0 1 1-.812-.813L6.688 7.5l-3.47-3.469l-.073-.09a.575.575 0 0 1 .796-.797l.09.075L7.5 6.687z"/>
+            </svg>
         </button>
     </form>
 </dialog>
+{/if}
+
 
 <style>
     dialog {
-        container-type: inline-size;
-        position: relative;
+        &:not([open]) {
+            display: none;
+        }
+
+        position: fixed;
+        inset: 50% auto auto 50%;
+        transform: translate(-50%, -50%);
+
+        z-index: 1000;
         overflow: hidden;
-        /* width: 20rem; */
-        /* height: 20rem; */
+
         max-width: 25rem;
         min-width: 15rem;
         width: 50%;
-        margin: auto;
-        padding: 1rem;
 
+        padding: 1rem;
         border: none;
         border-radius: .5rem;
+
+        background-color: white;
 
         img {
             width: 60%;
