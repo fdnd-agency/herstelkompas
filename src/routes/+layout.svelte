baec8df0e860b2a721106b24b677f0b5fcb1c9c5
DivaniNL <dylanvannierop@gmail.com>
2026-01-21 15:36:26 +0100

    local fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -48,8 +48,6 @@
 	<link rel="icon" href="/img/favicon.ico"/>
 	<link rel="stylesheet" href="/css/stylesheet.css" />
 	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="/css/globals.css">
 </svelte:head>
 <div id="container">
